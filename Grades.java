11b2bb5f5a96d1fe968a248e71ee45e60ca7b373
Archer <2007010@student.acalanes.k12.ca.us>
2017-10-10 12:26:18 -0700

    initial commit

---
+++ Grades.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Grades here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Grades
+{
+    public static void main (String[] args) {
+    System.out.println("------------------------------------------");
+    System.out.println ("==\t\tStudent Points\t\t==");
+    System.out.println("------------------------------------------");
+    System.out.println ("Name\t\tLab\tBonus\tTotal");
+    System.out.println ("----\t\t---\t-----\t-----");
+    System.out.println ("Nick\t\t42\t7\t49");
+    System.out.println ("Scott\t\t52\t8\t60");
+    System.out.println ("Eddie\t\t38\t6\t44");
+    System.out.println ("Eric\t\t45\t10\t55");
+    System.out.println ("Parker\t\t47\t7\t54");
+    
+    
+    
+    
+}
+}
---
+++ Names.java
@@ -0,0 +1,16 @@
+public class Names
+{
+        // ------------------------
+        // main prints the list
+        // ------------------------
+        public static void main (String[] args)
+        {
+         System.out.println ();
+         System.out.println ("\tName\t\tHometown\t\tMajor");
+         System.out.println ("\t====\t\t========\t\t=====");
+         System.out.println ("\tAlex\t\tLafayette\t\tEngineering");
+         System.out.println ("\tNick\t\tPiedmont\t\tMath");
+         System.out.println ("\tScott\t\tSan Francisco\t\tEnglish");
+         System.out.println ();
+        }
+}
